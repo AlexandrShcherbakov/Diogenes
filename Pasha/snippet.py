f175d275f5c430ecfa05082dacc764dfdb305880
Pavel <pashakovalenko@yandex.ru>
2015-06-08 23:15:01 +0300

    snippets

--- Pasha/snippet.py
+++ Pasha/snippet.py
@@ -11,13 +11,14 @@ def ends(s):
 	for i in range(len(s) - 3):
 		if s[i] == '?' or s[i] == '!':
 			res[i] = 1
-		elif s[i] == '.' and s[i - 2] != ' ' and s[i + 1] == ' ' and s[i + 2].isupper():
+		elif s[i] == '.' and s[i - 2] != ' ' and s[i + 1] == ' ' and not s[i + 2].islower():
 				res[i] = 1
 	return res
 
 
 def get(req, title, url):
-	path = './1_1000/'
+	#path = './1_1000/'
+	path = '/media/pavel/Seagate Backup Plus Drive/IR/all/'
 	f = open(path + 'shift.txt', 'r')
 	fl = {}
 	sh = {}
@@ -37,7 +38,10 @@ def get(req, title, url):
 		html_text = zlib.decompress(base64.b64decode(data)).decode('utf-8')
 		
 		x = re.search('<title>.*</title>', html_text).span()
-		title[i] = html_text[x[0] + 7: x[1] - 8]
+		t = html_text[x[0] + 7: x[1] - 8]
+		while t.find(':') > 0:
+			t = t[t.find(':') + 1:]
+		title[i] = t
 
 		try:
 			x = html_text.find('b-topic__title-image')
@@ -71,7 +75,7 @@ def get(req, title, url):
 		req[i] = text
 		#print(text)
 
-def snip(lst, length=150):
+def snip(lst, length=100):
 	req = {}
 	title = {}
 	url = {}
@@ -83,6 +87,7 @@ def snip(lst, length=150):
 		i = 2
 		pos = -1
 		text = req[t[1]]
+		#print(text)
 		while True:
 			temp = '.{,%d}' % i
 			temp = temp.join(t[0])
@@ -93,10 +98,11 @@ def snip(lst, length=150):
 				break
 			else:
 				i += 1
+		#print(pos)
 		dots = ends(text)
 		r = ''
 		p = pos
-		while (not dots[p - 1]) and (pos - p < 100):
+		while (not dots[p - 1]) and (pos - p < 100) and (p > 10):
 			p -= 1
 		if not dots[p - 1]:
 			while text[p - 1] != ' ':
@@ -106,7 +112,7 @@ def snip(lst, length=150):
 			if dots[i]:
 				r = text[pos : i]
 				break
-		i = pos + length + 50
+		i = pos + length
 		while r == '':
 			i += 1
 			if text[i] == ' ':
@@ -122,8 +128,9 @@ def snip(lst, length=150):
 def main():
 	#req = [(['железнодорожн', 'вокзал', 'в', 'качеств', 'диспле', 'для', 'показ', 'реклам', 'он', 'поступ',
 	#	'в', 'продаж', '21', 'сентябр'], 100)]
-	req = [(['игра'], i) for i in range(41, 42)]
-	snip(req, 300)
+	#req = [(['игра'], i) for i in range(41, 42)]
+	req = [[['медвед', 'владимир', 'путин', 'выступ', 'с', 'послан', 'к', 'федеральн', 'собран', 'в', 'четверг', '4'], 453128], [['медвед', 'призва', 'голосова', 'за', 'един', 'росс', 'и', 'путин', 'главн', 'росс', 'мир', 'бывш', 'ссср', 'финанс', 'бизнес', 'силов', 'структур'], 477134], [['путин', 'об', 'эт', 'медвед', 'заяв', 'в', 'интерв', 'трем', 'федеральн', 'канал', '29', 'сентябр', 'если'], 88304], [['медвед', 'пообеща', 'избав', 'росс', 'от', 'ненужн', 'импорт', 'lenta', 'ru', '10', 'декабр', '2014', 'путин', 'назва', 'рост', 'цен', 'на', 'продукт', 'времен', 'lenta', 'ru', '09'], 261248], [['медвед', 'захват', 'проходн', 'комбинат', '00', '21', 'сегодн', 'путин', 'рассказа', 'о', 'рецепт', 'счастлив', 'нов', 'год', '14', '44', '30'], 30763], [['медвед', 'был', 'водвор', 'обратн', 'в', 'зоопарк', 'обсуд', 'последн', 'новост', '13', '42', 'путин', 'поддержа', 'выделен', 'земл', 'жител', 'дальн', 'восток', '13', '50', 'подсчита'], 332445]]
+	print(snip(req))
 
 if __name__ == '__main__':
 	main()
